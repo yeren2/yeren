6e80e17d004963c1c188406559bfdf166cbe8a92
yeren2 <153232427+yeren2@users.noreply.github.com>
2023-12-07 23:31:18 +0300

    Add files via upload

---
+++ deneme2.1.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("boyunuz:");
+    scanf("%d",&x);
+    printf("okudugunuz okul:");
+    scanf("%d" , &x);
+    printf("AMINA KODUMUN GAYI");
+
+}
